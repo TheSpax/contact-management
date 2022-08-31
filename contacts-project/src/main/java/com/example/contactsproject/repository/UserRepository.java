853ba7ba32ae8248f49ae9be75abb9b9126394af
TheSpax <jovan.joca.spasic@gmail.com>
2022-08-31 16:47:43 +0200

    docker files added

--- contacts-project/src/main/java/com/example/contactsproject/repository/UserRepository.java
+++ contacts-project/src/main/java/com/example/contactsproject/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.example.contactsproject.repository;
 
 import com.example.contactsproject.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
